304ce46e6e7f0fb8f1e0381306798e59739c5f2f
Mohamed Abd Elhakim <mohammedabdelhakim98@gmail.com>
2026-02-06 20:16:31 +0200

    add firebase to project.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'app.dart';
 import 'core/theme/theme_cubit/theme_cubit.dart';
 import 'core/theme/theme_service.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(
     BlocProvider(
       create: (context) {
