656c920c817f3ca6cc279daf0a84fc4842f2cd18
Nada Elmadawy <nadaelmadawy61@gmail.com>
2026-02-13 17:47:06 -0800

    HomeView_end_without_ontap

--- lib/features/home_view.dart
+++ lib/features/home_view.dart
@@ -1,209 +1,302 @@
 import 'package:flutter/material.dart';
 
-import '../core/constants/app_colors.dart';
-
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SingleChildScrollView(
-        padding: const EdgeInsets.all(24),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            // الإحصائيات
-            Row(
-              children: [
-                Expanded(
-                  child: _buildStatCard(
-                    title: 'إجمالي الإنتاج',
-                    value: '153',
-                    icon: Icons.shopping_bag,
-                    color: Colors.red,
-                  ),
-                ),
-                // const SizedBox(width: AppSpacing.lg),
-                Expanded(
-                  child: _buildStatCard(
-                    title: 'نسبة الزيادة',
-                    value: '+12%',
-                    icon: Icons.trending_up,
-                    color: AppColors.darkCard,
-                  ),
-                ),
-              ],
-            ),
-            // const SizedBox(height: AppSpacing.xl),
+      backgroundColor: const Color(0xffF5F6FA),
 
-            // الأقسام
-            Text(
-              'الأقسام',
-              // style: AppTextStyles.heading3,
-            ),
-            // const SizedBox(height: AppSpacing.md),
-            GridView.count(
-              crossAxisCount: 2,
-              shrinkWrap: true,
-              physics: const NeverScrollableScrollPhysics(),
-              // mainAxisSpacing: AppSpacing.lg,
-              // crossAxisSpacing: AppSpacing.lg,
-              children: [
-                _buildSectionCard(
-                  title: 'المخزن الرئيسي',
-                  subtitle: '450 صنف متنوع',
-                  icon: Icons.warehouse,
-                  onTap: () {
-                    // setState(() => _currentIndex = 1);
-                  },
-                ),
-                _buildSectionCard(
-                  title: 'الفواتير',
-                  subtitle: '12 فاتورة معلقة',
-                  icon: Icons.receipt,
-                  onTap: () {},
-                ),
-                _buildSectionCard(
-                  title: 'حضور وانصراف',
-                  subtitle: '85 موظف حاضر',
-                  icon: Icons.people,
-                  onTap: () {},
-                ),
-                _buildSectionCard(
-                  title: 'التقارير',
-                  subtitle: 'آخر التحديثات',
-                  icon: Icons.bar_chart,
-                  onTap: () {
-                    // setState(() => _currentIndex = 3);
-                  },
-                ),
-              ],
-            ),
-            // const SizedBox(height: AppSpacing.xl),
-
-            // آخر التحديثات
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Text(
-                  'آخر التحديثات',
-                  // style: AppTextStyles.heading3
-                ),
-                TextButton(onPressed: () {}, child: const Text('عرض الكل')),
-              ],
-            ),
-            // const SizedBox(height: AppSpacing.md),
-            _buildUpdateItem('استيلام قياس جديد', 'منذ 30 دقيقة', Icons.shopping_cart),
-            // const SizedBox(height: AppSpacing.md),
-            _buildUpdateItem('تسجيل حضور صباحي', 'منذ ساعة', Icons.check_circle),
-            // const SizedBox(height: AppSpacing.md),
-            _buildUpdateItem('اعتماد فاتورة #9022', 'منذ ساعة', Icons.verified),
-          ],
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: ListView(
+            children: const [
+              SizedBox(height: 10),
+              Header(),
+              SizedBox(height: 20),
+              ProductionCard(),
+              SizedBox(height: 20),
+              GridSection(),
+              SizedBox(height: 20),
+              UpdatesSection(),
+              SizedBox(height: 30),
+            ],
+          ),
         ),
       ),
     );
   }
+}
 
-  Widget _buildStatCard({
-    required String title,
-    required String value,
-    required IconData icon,
-    required Color color,
-  }) {
+////////////////////////////////////////////////////////////
+/// Header / appbar
+////////////////////////////////////////////////////////////
+
+class Header extends StatelessWidget {
+  const Header({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        const CircleAvatar(radius:27 , backgroundImage: AssetImage("assets/images/logo_img.png"),),
+        const SizedBox(width: 10),
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: const [
+            Text(
+              "هاي كلاس",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            Text("نظام إدارة المصنع"),
+          ],
+        ),
+        const Spacer(),
+        const CircleAvatar(child: Icon(Icons.person)),
+        const SizedBox(width: 10),
+        const CircleAvatar(child: Icon(Icons.notifications)),
+
+      ],
+    );
+  }
+}
+
+////////////////////////////////////////////////////////////
+/// Production Card
+////////////////////////////////////////////////////////////
+
+class ProductionCard extends StatelessWidget {
+  const ProductionCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(16),
+      height: 150,
+      padding: const EdgeInsets.all(20),
       decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-        // boxShadow: [AppShadows.md],
-      ),
-      child: Column(
-        children: [
-          Icon(icon, color: color, size: 32),
-          const SizedBox(height: 12),
-          Text(
-            value,
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold, color: color),
-          ),
-          const SizedBox(height: 12),
-          Text(
-            title,
-            // style: AppTextStyles.caption,
-            textAlign: TextAlign.center,
-          ),
+        borderRadius: BorderRadius.circular(25),
+        gradient: const LinearGradient(
+          colors: [
+            Colors.black,
+            Color(0xff3A1C1C),
+          ],
+        ),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 10,
+            color: Colors.black.withOpacity(.2),
+            offset: const Offset(0, 5),
+          )
         ],
       ),
-    );
-  }
-
-  Widget _buildSectionCard({
-    required String title,
-    required String subtitle,
-    required IconData icon,
-    required VoidCallback? onTap,
-  }) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        padding: const EdgeInsets.all(15),
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(12),
-          // boxShadow: [AppShadows.md],
-          border: Border.all(color: Colors.transparent),
-        ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(icon, size: 40, color: AppColors.darkCard),
-            // const SizedBox(height: AppSpacing.md),
-            Text(
-              title,
-              // style: AppTextStyles.heading3,
-              textAlign: TextAlign.center,
-            ),
-            // const SizedBox(height: AppSpacing.sm),
-            Text(
-              subtitle,
-              // style: AppTextStyles.caption,
-              textAlign: TextAlign.center,
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-
-  Widget _buildUpdateItem(String title, String time, IconData icon) {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(12),
-        // boxShadow: [AppShadows.sm],
-      ),
-      child: Row(
-        children: [
-          Icon(icon, color: AppColors.primaryRed, size: 24),
-          // const SizedBox(width: AppSpacing.lg),
-          Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  title,
-                  // style: AppTextStyles.body1
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const [
+          Text(
+            "إجمالي الإنتاج اليومي",
+            style: TextStyle(color: Colors.white70, fontSize: 20 , fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 20),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                "1,280",
+                style: TextStyle(
+                  fontSize: 34,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
                 ),
-                Text(
-                  time,
-                  // style: AppTextStyles.caption
-                ),
-              ],
-            ),
+              ),
+              SizedBox(width: 8),
+              Text(
+                "قطعة",
+                style: TextStyle(color: Colors.amber, fontSize: 18),
+              ),
+            ],
           ),
         ],
       ),
     );
   }
 }
+
+////////////////////////////////////////////////////////////
+/// Grid Section / هنا الاستدعاء فقط اما الاساسى تحت
+////////////////////////////////////////////////////////////
+
+class GridSection extends StatelessWidget {
+  const GridSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GridView.count(
+      physics: const NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      crossAxisCount: 2,
+      mainAxisSpacing: 15,
+      crossAxisSpacing: 15,
+      childAspectRatio: 1.1,
+      children: const [
+        DashboardCard(
+          icon: Icons.inventory_2,
+          title: "المخزن الرئيسي",
+          subtitle: "450 صنف متوفر",
+          status: "",
+        ),
+        DashboardCard(
+          icon: Icons.receipt_long,
+          title: "الفواتير",
+          subtitle: "12 فاتورة معلقة",
+          status: "",
+          statusColor: Colors.orange,
+        ),
+        DashboardCard(
+          icon: Icons.badge,
+          title: "حضور وانصراف",
+          subtitle: "85 موظف حاضر",
+          status: "",
+        ),
+        DashboardCard(
+          icon: Icons.payments,
+          title: "الرواتب",
+          subtitle: "تجهيز كشوف الشهر",
+          status: "",
+          statusColor: Colors.green,
+        ),
+      ],
+    );
+  }
+}
+
+////////////////////////////////////////////////////////////
+/// Dashboard Card / هنا الاساسى اللى بنستدعى منه فوق
+////////////////////////////////////////////////////////////
+
+class DashboardCard extends StatelessWidget {
+  final IconData icon;
+  final String title;
+  final String subtitle;
+  final String status;
+  final Color? statusColor;
+
+  const DashboardCard({
+    super.key,
+    required this.icon,
+    required this.title,
+    required this.subtitle,
+    required this.status,
+    this.statusColor,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.all(18),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(25),
+        boxShadow: [
+          BoxShadow(
+            blurRadius: 10,
+            color: Colors.black.withOpacity(.05),
+          )
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 25,
+            backgroundColor: Colors.grey.shade200,
+            child: Icon(icon),
+          ),
+          SizedBox(height: 20,),
+          Text(title,
+              style:
+              const TextStyle(fontWeight: FontWeight.bold, fontSize: 16)),
+          Text(subtitle, style: const TextStyle(color: Colors.grey)),
+          if (status.isNotEmpty)
+            Text(
+              status,
+              style: TextStyle(
+                color: statusColor ?? Colors.grey,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
+
+////////////////////////////////////////////////////////////
+/// Updates Section / اخر التحديثات
+////////////////////////////////////////////////////////////
+
+// الاستدعاء فقط اما الاساسى تحت
+
+class UpdatesSection extends StatelessWidget {
+  const UpdatesSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: const [
+        Text(
+          "آخر التحديثات",
+          style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 10),
+        UpdateTile(
+          title: "استلام قماش قطني",
+          subtitle: "تم إضافة 50 رول للمخزن",
+          time: "منذ 5 د",
+        ),
+        UpdateTile(
+          title: "تسجيل حضور صباحي",
+          subtitle: "تم اكتمال حضور وردية الصباح",
+          time: "منذ 25 د",
+        ),
+        UpdateTile(
+          title: "اعتماد فاتورة #9022",
+          subtitle: "بواسطة الإدارة المالية",
+          time: "منذ ساعة",
+        ),
+      ],
+    );
+  }
+}
+
+// هنا الاساسى اللى بنستدعى منه فوق
+class UpdateTile extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final String time;
+
+  const UpdateTile({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.time,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      tileColor: Colors.white,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(15),
+      ),
+      contentPadding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+      leading: const Icon(Icons.check_circle_outline),
+      title: Text(title, textAlign: TextAlign.right),
+      subtitle: Text(subtitle, textAlign: TextAlign.right),
+      trailing: Text(time),
+    );
+  }
+}
+
