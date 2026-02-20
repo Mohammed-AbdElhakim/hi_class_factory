ae1830074b8573d8faf9dad1d5579400fc60ddb2
Mohamed Abd Elhakim <mohammedabdelhakim98@gmail.com>
2026-02-20 17:29:27 +0200

    refactor code.

--- lib/features/employee/presentation/views/employee_view.dart
+++ lib/features/employee/presentation/views/employee_view.dart
@@ -1,24 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hi_class_factory/core/responsive/adaptive_layout.dart';
 import 'package:hi_class_factory/features/employee/presentation/layouts/employee_mobile_layout.dart';
-
-import '../../../../core/constants/service_locator.dart';
-import '../../data/repositories/employee_repo_impl.dart';
-import '../manager/employee_cubit.dart';
 
 class EmployeeView extends StatelessWidget {
   const EmployeeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (_) => EmployeeCubit(getIt.get<EmployeeRepoImpl>())..getEmployee(),
-      child: AdaptiveLayout(
-        mobilLayout: (context) => EmployeeMobileLayout(),
-        tabletLayout: (context) => EmployeeMobileLayout(),
-        desktopLayout: (context) => EmployeeMobileLayout(),
-      ),
+    return AdaptiveLayout(
+      mobilLayout: (context) => EmployeeMobileLayout(),
+      tabletLayout: (context) => EmployeeMobileLayout(),
+      desktopLayout: (context) => EmployeeMobileLayout(),
     );
   }
 }
--- lib/features/navigationBar/presentation/layouts/mobile/navigation_bar_mobile_layout.dart
+++ lib/features/navigationBar/presentation/layouts/mobile/navigation_bar_mobile_layout.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../../../core/constants/service_locator.dart';
+import '../../../../employee/data/repositories/employee_repo_impl.dart';
+import '../../../../employee/presentation/manager/employee_cubit.dart';
 import '../../../../employee/presentation/views/employee_view.dart';
 import '../../../../home/presentation/views/home_view.dart';
 import '../../../../report/presentation/views/report_view.dart';
@@ -17,17 +21,21 @@ class _NavigationBarMobileLayoutState extends State<NavigationBarMobileLayout> {
   int currentIndexPage = 0;
 
   final List<Widget> _pages = [HomeView(), ReportView(), EmployeeView(), SittingView()];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: _pages[currentIndexPage],
-      bottomNavigationBar: CustomBottomNavigationBar(
-        currentIndexPage: currentIndexPage,
-        onTap: (value) {
-          setState(() {
-            currentIndexPage = value;
-          });
-        },
+    return BlocProvider(
+      create: (_) => EmployeeCubit(getIt.get<EmployeeRepoImpl>())..getEmployee(),
+      child: Scaffold(
+        body: _pages[currentIndexPage],
+        bottomNavigationBar: CustomBottomNavigationBar(
+          currentIndexPage: currentIndexPage,
+          onTap: (value) {
+            setState(() {
+              currentIndexPage = value;
+            });
+          },
+        ),
       ),
     );
   }
